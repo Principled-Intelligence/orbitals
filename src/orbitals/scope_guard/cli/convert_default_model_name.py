c872e782f520e911e54252990dfc9968c57167b1
poccio <luigi.p.procopio@gmail.com>
2026-04-02 16:08:46 +0200

    fix: wrong function name for cli command

--- src/orbitals/scope_guard/cli/convert_default_model_name.py
+++ src/orbitals/scope_guard/cli/convert_default_model_name.py
@@ -6,5 +6,5 @@ app = typer.Typer()
 
 
 @app.command("convert-default-model-name")
-def serve(model_name: str = typer.Argument(..., help="The model name")):
+def convert_default_model_name(model_name: str = typer.Argument(..., help="The model name")):
     print(ScopeGuard.maybe_map_model(model_name))
